f4f3039bb12796bea6ec50d01bb171667efdd749
Vitaliy Ugera <shnobel12@gmail.com>
2022-10-16 16:51:44 +0300

    Initial Code Commit
    Added the ASP.NET Core empty template project files.

---
+++ ASP.NET_Core-training-project/ASP.NET_Core-training-project/Extensions/AuthenticationExtensions.cs
@@ -0,0 +1,7 @@
+﻿public static class AuthenticationExtensions
+{
+    public static IApplicationBuilder UseDefinedAuthentication(this IApplicationBuilder builder, string pattern)
+    {
+        return builder.UseMiddleware<AuthenticationMiddleware>(pattern);
+    }
+}
---
+++ ASP.NET_Core-training-project/ASP.NET_Core-training-project/Middlewares/AuthenticationMiddleware.cs
@@ -0,0 +1,23 @@
+﻿public class AuthenticationMiddleware
+{
+    readonly RequestDelegate next;
+    private string pattern;
+    public AuthenticationMiddleware(RequestDelegate next, string pattern)
+    {
+        this.next = next;
+        this.pattern = pattern;
+    }
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var token = context.Request.Query["token"];
+        if (token != pattern)
+        {
+            context.Response.StatusCode = 403;
+            await context.Response.WriteAsync("Token is invalid");
+        }
+        else
+        {
+            await next.Invoke(context);
+        }
+    }
+}
